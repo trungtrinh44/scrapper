939d0e8f13ef7a3d48e714287aa169f8bf327cdc
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2019-05-09 15:28:02 +0700

    thanhnien spider

--- scrapper/spiders/thanhnien.py
+++ scrapper/spiders/thanhnien.py
@@ -4,60 +4,54 @@ import scrapy
 
 class ThanhNienSpider(scrapy.Spider):
     name = "thanhnien"
-    BASE_URL = 'http://thanhnien.vn'
+    BASE_URL = 'https://thanhnien.vn'
 
     def start_requests(self):
         url = self.BASE_URL
-        yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=url, callback=self.parse_first_level)
 
-    def parse(self, response):
-        links = response.css('#mainmenu a::attr(href)') .extract()
-        normal_links = [
-            self.BASE_URL + x for x in links if not x.startswith('http') and "javascript" not in x]
-        full_links = [x for x in links if x.startswith(
-            'http') and "media" not in x]
-        for link in full_links:
-            yield scrapy.Request(link, callback=self.parse_full_links)
+    def parse_first_level(self, response):
+        links = response.css('nav.site-header__nav a::attr(href)').extract()
+        normal_links = [self.BASE_URL + x for x in links if not x.startswith('http') and "javascript" not in x]
         for link in normal_links:
-            yield scrapy.Request(link, callback=self.parse_links)
+            yield scrapy.Request(link, callback=self.parse_second_level)
 
-    def parse_full_links(self, response):
-        links = response.css('#submenu a::attr(href)').extract()
-        for link in links:
-            yield scrapy.Request(link, callback=self.parse_links)
+    def parse_second_level(self, response):
+        for link in self.parse_third_level(response):
+            yield link
+        links = response.css('nav.site-header__nav a::attr(href)').extract()
+        normal_links = [self.BASE_URL + x for x in links if not x.startswith('http') and "javascript" not in x]
+        for link in normal_links:
+            yield scrapy.Request(link, callback=self.parse_third_level)
 
-    def parse_links(self, response):
-        links = response.css('article header a::attr(href)').extract()
-        paging = response.css(
-            '#paging ul li:last-child a::attr(href)').extract_first()
-        viewdate = response.css('a.viewdate-btn::attr(href)').extract_first()
-        if viewdate is not None:
-            yield scrapy.Request(url=self.BASE_URL + viewdate, callback=self.parse_links)
-        else:
-            links = list(map(lambda x: self.BASE_URL +
-                             x if not x.startswith('http') else x, links))
-            for link in links:
-                yield scrapy.Request(link, callback=self.parse_content)
-            if paging:
-                yield scrapy.Request(url=self.BASE_URL + paging, callback=self.parse_links)
+    def parse_third_level(self, response):
+        for link in self.parse_article_groups(response):
+            yield link
+        article_groups = response.css('div.cate-content div.zone--timeline')
+        nav_links = article_groups.css('nav#paging span#ctl00_main_ContentList1_pager ul.pagination li a::attr(href)').extract()
+        for link in nav_links:
+            yield scrapy.Request(self.BASE_URL + link, callback=self.parse_article_groups)
+
+    def parse_article_groups(self, response):
+        article_groups = response.css('div.cate-content div.zone--timeline')
+        article_links = article_groups.css('div.relative article.story a::attr(href)').extract()
+        for link in article_links:
+            yield scrapy.Request(self.BASE_URL + link, callback=self.parse_content)
 
     def parse_content(self, response):
-        title = response.css('.main-title::text').extract_first()
-        summary = response.css('#chapeau').xpath('.//text()').extract()
-        content = response.css('#abody').xpath('.//text()').extract()
-        cat = response.css('.sub a::text').extract_first()
-        date = response.css('.meta time::text').extract_first()
-        content = '\n'.join(content).lstrip().rstrip()
-        segmented_content = ViTokenizer.tokenize(content)
-        summary = '\n'.join(summary).lstrip().rstrip()
-        segmented_summary = ViTokenizer.tokenize(summary)
-        tags = response.xpath('//ul[@class="tags"]//a/text()').extract()
-        yield {'_id': response.url,
-               'date': date,
-               'title': title,
-               'summary': summary,
-               'segmented_summary': segmented_summary,
-               'content': content,
-               'segmented_content': segmented_content,
-               'type': cat,
-               'tags': tags}
+        article = response.css("div#storybox")
+        title = ' '.join(article.css("h1.details__headline").xpath('.//text()').extract())
+        summary = ' '.join(article.css("div.sapo").xpath('.//text()').extract())
+        content = '\n'.join(' '.join(x.strip() for x in div.xpath('.//text()[not(ancestor::td) and not(ancestor::article)]').extract() if x.strip() != "").strip() for div in article.css("div#main_detail div#abody").xpath("./div[not(contains(@class,'details__morenews')) and not(contains(@class,'imgcaption'))] | ./h3 | ./p"))
+        cat = ', '.join(response.css('div.breadcrumbs').xpath('//span[@itemprop="title"]').css('::text').extract())
+        date = ' '.join(response.css('div.details__meta div.meta time::text').extract())
+        # tags = response.xpath('//ul[@class="tags"]//a/text()').extract()
+        yield {
+            '_id': response.url,
+            'date': date,
+            'title': title,
+            'summary': summary,
+            'content': content,
+            'type': cat,
+            # 'tags': tags
+        }
